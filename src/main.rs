3593d4f1789b70b8bf46166fc1fa32f5e5e4ac20
ethancarlsson <ethanmcarlsson@gmail.com>
2023-12-24 12:13:51 +0100

    Convert GET to simple Hurl file
    These are just basic files that expect 200 responses requires manual setup
    of a variables file.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,79 @@
-use crate::cli::Cli;
+use std::{fs::File, io::Write, path::PathBuf};
+
+use crate::{cli::Cli, oai_path::to_hurl_files};
+use anyhow::{bail, Context, Result};
 use clap::Parser;
 
 mod cli;
+mod oai_path;
 
-fn main() {
+fn main() -> Result<()> {
     let args = Cli::parse();
 
-    println!("path: {:?}", args.path)
+    let hurl_files = hurl_files_from_spec_path(args.path)?;
+    for file_contents in hurl_files {
+        let file_path = format!("{}/{}.hurl", args.out.display(), file_contents.0);
+        let mut file = File::create(&file_path)
+            .with_context(|| format!("Could not open new file at {file_path}"))?;
+        file.write_all(file_contents.1.as_bytes())
+            .with_context(|| format!("could not write to file at {file_path}"))?;
+    }
+
+    Ok(())
+}
+
+fn hurl_files_from_spec_path(path: PathBuf) -> Result<Vec<(String, String)>, anyhow::Error> {
+    let spec = oas3::from_path(path).with_context(|| format!("Issue with specification"))?;
+
+    let mut files = vec![];
+    for path in spec.paths.iter() {
+        let hurl_files = to_hurl_files(path, &spec, &spec.components);
+
+        if hurl_files.errors.len() > 0 {
+            bail!(
+                "Found errors while parsing openapi file:\n\n{}",
+                hurl_files
+                    .errors
+                    .iter()
+                    .map(|e| e.to_string())
+                    .collect::<Vec<String>>()
+                    .join("\n")
+            )
+        }
+
+        for file in hurl_files.hurl_files {
+            files.push((
+                path.0.replace("/", "_"),
+                (hurlfmt::format::format_text(file, false)),
+            ));
+        }
+    }
+
+    Ok(files)
+}
+
+#[cfg(test)]
+mod tests {
+    use std::{path::PathBuf, str::FromStr};
+
+    use crate::hurl_files_from_spec_path;
+
+    #[test]
+    fn hurl_files_from_spec_path_with_pet_store_spec() {
+        let result =
+            hurl_files_from_spec_path(PathBuf::from_str("test_files/pet_store.json").unwrap());
+
+        let expected: Vec<(String, String)> = vec![
+            (
+                "_pets".to_string(),
+                "GET {{host}}/pets?limit=3  \n\nHTTP 200\n".to_string(),
+            ),
+            (
+                "_pets_{petId}".to_string(),
+                "GET {{host}}/pets/string_value  \n\nHTTP 200\n".to_string(),
+            ),
+        ];
+
+        assert_eq!(result.unwrap(), expected);
+    }
 }
---
+++ src/oai_path.rs
@@ -0,0 +1,238 @@
+use hurl_core::ast::{
+    Entry, HurlFile, LineTerminator, Method, Pos, Request, Response, SourceInfo, Status, Template,
+    TemplateElement, Version, VersionValue, Whitespace,
+};
+use oas3::{
+    spec::{Components, FromRef, ObjectOrReference, Parameter, PathItem, RefError, SchemaType},
+    Schema, Spec,
+};
+
+type OApiPath<'a> = (&'a String, &'a PathItem);
+
+pub struct HurlFiles {
+    pub hurl_files: Vec<HurlFile>,
+    pub errors: Vec<RefError>,
+}
+
+pub fn to_hurl_files(path: OApiPath, spec: &Spec, components: &Option<Components>) -> HurlFiles {
+    let mut hurl_files = vec![];
+    let mut errors = vec![];
+
+    // TODO: implement other HTTP methods
+    match to_get_file(path, &spec, components) {
+        Ok(file) => match file {
+            Some(f) => hurl_files.push(f),
+            None => (),
+        },
+        Err(e) => errors.extend(e),
+    }
+
+    return HurlFiles { hurl_files, errors };
+}
+
+fn to_get_file(
+    path: OApiPath,
+    spec: &Spec,
+    _components: &Option<Components>,
+) -> Result<Option<HurlFile>, Vec<RefError>> {
+    let operation = match &path.1.get {
+        Some(o) => o,
+        None => return Ok(None),
+    };
+
+    let param_result_iter = operation.parameters.iter().map(|p| match p {
+        ObjectOrReference::Object(p) => Ok(p.clone()),
+        ObjectOrReference::Ref { ref_path } => Parameter::from_ref(&spec, &ref_path),
+    });
+
+    let errors = param_result_iter
+        .clone()
+        .filter_map(|p| match p {
+            Ok(_) => None,
+            Err(e) => Some(e),
+        })
+        .collect::<Vec<RefError>>();
+
+    if errors.len() > 0 {
+        return Err(errors);
+    }
+
+    let param_iter = param_result_iter.clone().filter_map(|p| match p {
+        Ok(p) => Some(p),
+        Err(_) => None,
+    });
+
+    let path_params = param_iter.clone().filter(|p| p.location == "path");
+    let mut query_params = param_iter.filter(|p| p.location == "query");
+
+    let uri = path_params.fold(path.0.clone(), |uri, param| {
+        let schema = &param.schema.unwrap_or(Schema::default());
+        uri.replace(
+            &("{".to_string() + &param.name + "}"),
+            path_param_from_schema_type(schema.schema_type.unwrap_or(SchemaType::String)),
+        )
+    });
+
+    let uri_with_first_query_param = format!(
+        "{uri}{}",
+        match query_params.next() {
+            Some(param) => {
+                let schema = param.schema.unwrap_or(Schema {
+                    example: None,
+                    ..Schema::default()
+                });
+                format!(
+                    "?{}={}",
+                    param.name,
+                    match schema.example {
+                        Some(e) => e.to_string().replace("\"", ""),
+                        None => path_param_from_schema_type(
+                            schema.schema_type.unwrap_or(SchemaType::String)
+                        )
+                        .to_string(),
+                    }
+                )
+            }
+            None => "".to_string(),
+        }
+    );
+
+    let uri_with_query_params = query_params.fold(uri_with_first_query_param, |uri, param| {
+        format!(
+            "{uri}&{}={}",
+            param.name,
+            path_param_from_schema_type(
+                param
+                    .schema
+                    .unwrap_or(Schema::default())
+                    .schema_type
+                    .unwrap_or(SchemaType::String)
+            )
+        )
+    });
+
+    let entry = Entry {
+        request: Request {
+            line_terminators: vec![],
+            space0: Whitespace {
+                value: "".to_string(),
+                source_info: empty_source_info(),
+            },
+            method: Method(HttpMethod::GET.to_string()),
+            space1: Whitespace {
+                value: " ".to_string(),
+                source_info: empty_source_info(),
+            },
+            url: Template {
+                delimiter: None,
+                elements: vec![TemplateElement::String {
+                    value: "".to_string(),
+                    encoded: format!("{{{{host}}}}{uri_with_query_params}"),
+                }],
+                source_info: empty_source_info(),
+            },
+            line_terminator0: LineTerminator {
+                space0: Whitespace {
+                    value: " ".to_string(),
+                    source_info: empty_source_info(),
+                },
+                comment: None,
+                newline: Whitespace {
+                    value: " ".to_string(),
+                    source_info: empty_source_info(),
+                },
+            },
+            headers: vec![],
+            sections: vec![],
+            body: None,
+            source_info: empty_source_info(),
+        },
+        response: Some(Response {
+            line_terminators: vec![newline(), newline()],
+            version: Version {
+                value: VersionValue::VersionAny,
+                source_info: empty_source_info(),
+            },
+            space0: Whitespace {
+                value: "".to_string(),
+                source_info: empty_source_info(),
+            },
+            status: Status {
+                value: hurl_core::ast::StatusValue::Specific(200),
+                source_info: empty_source_info(),
+            },
+            space1: Whitespace {
+                value: " ".to_string(),
+                source_info: empty_source_info(),
+            },
+            line_terminator0: newline(),
+            headers: vec![],
+            sections: vec![],
+            body: None,
+            source_info: empty_source_info(),
+        }),
+    };
+
+    Ok(Some(HurlFile {
+        entries: vec![entry],
+        line_terminators: vec![],
+    }))
+}
+
+fn path_param_from_schema_type(schema_type: SchemaType) -> &'static str {
+    match schema_type {
+        SchemaType::Boolean => "true",
+        SchemaType::Integer => "3",
+        SchemaType::Number => "5.5",
+        SchemaType::String => "string_value",
+        SchemaType::Array => "[]array_value",
+        SchemaType::Object => "{}",
+    }
+}
+
+fn empty_source_info() -> SourceInfo {
+    SourceInfo {
+        start: Pos { column: 0, line: 0 },
+        end: Pos { column: 0, line: 0 },
+    }
+}
+
+fn newline() -> LineTerminator {
+    LineTerminator {
+        space0: Whitespace {
+            value: "".to_string(),
+            source_info: empty_source_info(),
+        },
+
+        comment: None,
+        newline: Whitespace {
+            value: "\n".to_string(),
+            source_info: SourceInfo {
+                start: Pos { column: 0, line: 0 },
+                end: Pos { column: 0, line: 0 },
+            },
+        },
+    }
+}
+
+enum HttpMethod {
+    GET,
+    // PUT,
+    // POST,
+    // PATCH,
+    // OPTIONS,
+    // HEAD,
+}
+
+impl HttpMethod {
+    fn to_string(self) -> String {
+        match self {
+            HttpMethod::GET => "GET".to_string(),
+            // HttpMethod::PUT => "PUT".to_string(),
+            // HttpMethod::POST => "POST".to_string(),
+            // HttpMethod::PATCH => "PATCH".to_string(),
+            // HttpMethod::OPTIONS => "OPTIONS".to_string(),
+            // HttpMethod::HEAD => "HEAD".to_string(),
+        }
+    }
+}
