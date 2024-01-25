5c7f923dd61e0e1af2e7bc63d942e6f2146ef483
ethancarlsson <ethanmcarlsson@gmail.com>
2024-01-25 16:30:23 +0100

    ci: Add caching to changelog
    Also debug pre-changelog gen

--- changelog/pre-changelog-gen.js
+++ changelog/pre-changelog-gen.js
@@ -11,4 +11,5 @@ exports.preTagGeneration = function (tag) {
 	`,
     (error, stdout, stderr) => console.log(`exec_result: err: ${error}, output: ${stdout}, stderr ${stderr}`),
   );
+	console.log(__dirname)
 };
