a3fa7c6c6f30e0bfa5d53846484fb14b2e96a3d8
ethancarlsson <ethanmcarlsson@gmail.com>
2024-01-25 16:35:48 +0100

    ci: Fixes CI by using correct path to executable

--- changelog/pre-changelog-gen.js
+++ changelog/pre-changelog-gen.js
@@ -4,12 +4,11 @@ exports.preTagGeneration = function (tag) {
   exec(
     `
 	{
-		hyperfine --warmup 50 'target/release/openapi-to-hurl test_files/pet_store_advanced.json --output-to console' -u millisecond --shell=none &&
+		hyperfine --warmup 50 '../target/release/openapi-to-hurl ../test_files/pet_store_advanced.json --output-to console' -u millisecond --shell=none &&
 		git log --format="%H" -n 1 &&
 		echo ${tag};
 	} | python3 format_benchmarks.py >> bench_over_time.csv
 	`,
     (error, stdout, stderr) => console.log(`exec_result: err: ${error}, output: ${stdout}, stderr ${stderr}`),
   );
-	console.log(__dirname)
 };
