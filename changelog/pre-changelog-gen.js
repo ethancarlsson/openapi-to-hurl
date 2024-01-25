e90b2eee6f53f9d982adb10ea14d3891a53b7428
ethancarlsson <ethanmcarlsson@gmail.com>
2024-01-25 15:59:21 +0100

    ci: Causes benchmarks to trigger on version bump
    This will help keep track of performance and catch regressions

---
+++ changelog/pre-changelog-gen.js
@@ -0,0 +1,14 @@
+const { exec } = require("child_process");
+
+exports.preTagGeneration = function (tag) {
+  exec(
+    `
+	{
+		hyperfine --warmup 50 'target/release/openapi-to-hurl test_files/pet_store_advanced.json --output-to console' -u millisecond --shell=none &&
+		git log --format="%H" -n 1 &&
+		echo ${tag};
+	} | python3 format_benchmarks.py >> bench_over_time.csv
+	`,
+    (error, stdout, stderr) => console.log(`exec_result: err: ${error}, output: ${stdout}, stderr ${stderr}`),
+  );
+};
