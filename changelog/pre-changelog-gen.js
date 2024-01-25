0e33e489a3010eec123808415d8518b7d8b2a420
ethancarlsson <ethanmcarlsson@gmail.com>
2024-01-25 17:41:08 +0100

    ci: Update bench to use await
    Otherwise benchmarking might finish after commit

--- changelog/pre-changelog-gen.js
+++ changelog/pre-changelog-gen.js
@@ -1,24 +1,32 @@
-const { exec } = require("child_process");
+const util = require("util");
+const exec = util.promisify(require("child_process").exec);
 
-exports.preTagGeneration = function (tag) {
+exports.preTagGeneration = async function (tag) {
   // Run the command a couple times to warm it up
-  for (let i = 0; i < 10; i++) {
-    exec(
-      "/home/runner/work/openapi-to-hurl/openapi-to-hurl/target/release/openapi-to-hurl /home/runner/work/openapi-to-hurl/openapi-to-hurl/test_files/pet_store_advanced.json --output-to console' -u millisecond --shell=none ",
-    );
+  for (let i = 0; i < 5; i++) {
+    try {
+      await exec(
+        "/home/runner/work/openapi-to-hurl/openapi-to-hurl/target/release/openapi-to-hurl /home/runner/work/openapi-to-hurl/openapi-to-hurl/test_files/pet_store_advanced.json --output-to console' -u millisecond --shell=none",
+      );
+    } catch (e) {
+      console.error(e);
+    }
   }
 
-  exec(
-    `
+  try {
+    const { stderr, stdout } = await exec(
+      `
 	{
 		hyperfine --warmup 50 '/home/runner/work/openapi-to-hurl/openapi-to-hurl/target/release/openapi-to-hurl /home/runner/work/openapi-to-hurl/openapi-to-hurl/test_files/pet_store_advanced.json --output-to console' -u millisecond --shell=none &&
 		git log --format="%H" -n 1 &&
 		echo ${tag};
 	} | python3 format_benchmarks.py >> bench_over_time.csv
 	`,
-    (error, stdout, stderr) =>
-      console.log(
-        `exec_result: err: ${error}, output: ${stdout}, stderr ${stderr}`,
-      ),
-  );
+    );
+    console.log(`output: ${stdout}, stderr ${stderr}`);
+  } catch (e) {
+    console.error(`err: ${e}`);
+  }
 };
+
+exports.preTagGeneration("test");
