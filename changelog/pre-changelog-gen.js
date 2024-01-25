26372bd50cba2541a1bf0abe06a7af90390f8c3e
ethancarlsson <ethanmcarlsson@gmail.com>
2024-01-25 16:57:25 +0100

    ci: Use cache for hyperfine and fix bench command

--- changelog/pre-changelog-gen.js
+++ changelog/pre-changelog-gen.js
@@ -4,11 +4,14 @@ exports.preTagGeneration = function (tag) {
   exec(
     `
 	{
-		hyperfine --warmup 50 '../target/release/openapi-to-hurl ../test_files/pet_store_advanced.json --output-to console' -u millisecond --shell=none &&
+		hyperfine --warmup 50 '/home/runner/work/openapi-to-hurl/openapi-to-hurl/target/release/openapi-to-hurl /home/runner/work/openapi-to-hurl/openapi-to-hurl/test_files/pet_store_advanced.json --output-to console' -u millisecond --shell=none &&
 		git log --format="%H" -n 1 &&
 		echo ${tag};
 	} | python3 format_benchmarks.py >> bench_over_time.csv
 	`,
-    (error, stdout, stderr) => console.log(`exec_result: err: ${error}, output: ${stdout}, stderr ${stderr}`),
+    (error, stdout, stderr) =>
+      console.log(
+        `exec_result: err: ${error}, output: ${stdout}, stderr ${stderr}`,
+      ),
   );
 };
