2905870dc5b79e34e7f2536123a0db9844e6a893
ethancarlsson <ethanmcarlsson@gmail.com>
2024-02-21 20:11:19 +0100

    refactor: Change and rename cli arguments
    BREAKING CHANGE: A number of arguments and options have been renamed.
    - `select-operation-id` -> `operation-id`
    - `variables-update-strategy` -> `variables-file-update`
    - `handle-errors` -> `error-handling`

--- src/cli.rs
+++ src/cli.rs
@@ -4,13 +4,13 @@ use clap::{Parser, ValueEnum};
 
 #[derive(ValueEnum, Clone, Default)]
 pub enum ResponseValidationChoice {
-    /// No Validation
+    /// No Validation.
     #[default]
     None,
-    /// Validates the result is any status code less than 400
+    /// Validates the result is any status code less than 400.
     NonErrorCode,
-    /// Validates the structure and types of the response body
-    /// NOTE: This tool will not produce response validation for union types (nullable, oneOf, not
+    /// Validates the structure and types of the response body.
+    /// Note: This tool will not produce response validation for union types (nullable, oneOf, not
     /// required, etc).
     Body,
     /// Validates the response body and treats all properties in the response body
@@ -20,27 +20,27 @@ pub enum ResponseValidationChoice {
 
 #[derive(ValueEnum, Clone, Default)]
 pub enum QueryParamChoice {
-    /// No query params
+    /// No query params.
     None,
+    /// Default values based on types.
     #[default]
-    /// Default values based on types
     Defaults,
 }
 
 #[derive(ValueEnum, Clone, Default)]
 pub enum VariablesUpdateStrategy {
-    /// Overwrites the entire variables file with new variables
+    /// Overwrites the entire variables file with new variables.
     Overwrite,
     #[default]
-    /// Merges new variables with old variables
+    /// Merges new variables with old variables.
     Merge,
 }
 
 #[derive(ValueEnum, Clone, Default)]
 pub enum Formatting {
-    /// Will not add any characters to the output that do not affect syntax
+    /// Will not add any characters to the output that do not affect syntax.
     NoFormatting,
-    /// Will add some formatting to the request body
+    /// Will add some formatting to the request body.
     #[default]
     RequestBodies,
 }
@@ -93,47 +93,49 @@ pub enum ErrorHandling {
 #[derive(Parser)]
 pub struct Cli {
     /// Input can be either a path to the specification or the result of stdin if used in a
-    /// pipeline
+    /// pipeline.
     pub input: Option<std::path::PathBuf>,
     /// If the `out-dir` argument is provided the output will go to a series of directories and
-    /// files instead of stdout
+    /// files instead of stdout.
+    /// Note: if now `out-dir` is given, no variables file will be created.
     #[arg(short = 'o', long)]
     pub out_dir: Option<std::path::PathBuf>,
-    /// This option indicates how the response should be validated
+    /// This option indicates how the response should be validated.
     #[arg(short = 'n', long, default_value_t = ResponseValidationChoice::default(), value_enum)]
     pub validation: ResponseValidationChoice,
-    /// Variables will be added to the hurl variables file and the header of each request. Format: `HEADER_KEY=HEADER_VALUE`
+    /// Variables will be added to the hurl variables file and the header of each request. Format: `HEADER_KEY=HEADER_VALUE`.
     #[arg(short = 'r', long, value_parser = parse_key_val::<String, String>)]
     pub header_vars: Vec<(String, String)>,
-    /// Lets you choose whether, and how to, pass query params
+    /// Choose whether, and how to, pass query params.
     #[arg(short = 'q', long, default_value_t = QueryParamChoice::default(), value_enum)]
     pub query_params: QueryParamChoice,
-    /// Select an operationId from Open API Spec, can select multiple operationIds
+    /// Generate only the operations identified with this options, e.g. `openapi-to-hurl openapi.json -i getProducts -i createProduct`.
     #[arg(short = 'i', long)]
-    pub select_operation_id: Option<Vec<String>>,
-    /// Filter by tags in the Open API Spec, can select multiple tags. If used with the
-    /// "select-operation-id" option the request will first be narrowed by tag then by operationId
+    pub operation_id: Option<Vec<String>>,
+    /// Generate only the operations under the given tags, e.g. `openapi-to-hurl openapi.json -t orders -t products`. 
+    /// If used with the `operation-id` option the generated files will first be narrowed by tag then by operationId.
     #[arg(short = 't', long)]
     pub tag: Option<Vec<String>>,
-    /// How the variables file should be updated
+    /// How the variables file should be updated.
     #[arg(long, default_value_t = VariablesUpdateStrategy::default(), value_enum)]
-    pub variables_update_strategy: VariablesUpdateStrategy,
-    /// How the output should be formatted
+    pub variables_file_update: VariablesUpdateStrategy,
+    /// How the output should be formatted.
     #[arg(long, default_value_t = Formatting::default(), value_enum)]
     pub formatting: Formatting,
-    /// Content type of the request. If the selected content type is not available in the schema or not
+    /// Desired content type of the request.
+    /// If the selected content type is not available in the schema or not
     /// supported by this tool the tool will select the first scpecified content type supported by this
     /// tool. If no valid content type is found the tool will use an empty request body.
     #[arg(long, default_value_t = CliContentType::default(), value_enum)]
     pub content_type: CliContentType,
     #[arg(short = 'l', long, default_value_t = LogLevel::default(), value_enum)]
     pub log_level: LogLevel,
-    /// Set this to true to silence all logging
+    /// Set this to true to silence all logging.
     #[arg(long, default_value_t = false)]
     pub quiet: bool,
-    /// Set to `log` to log errors and keep generating hurl files where possible.
+    /// How to handle recoverable errors.
     #[arg(long, default_value_t = ErrorHandling::default(), value_enum)]
-    pub handle_errors: ErrorHandling,
+    pub error_handling: ErrorHandling,
 }
 
 /// Parse a single key-value pair
