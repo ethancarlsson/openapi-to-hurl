c35e80e7361aef8ea400daceec7f834b8c470c4b
ethancarlsson <ethanmcarlsson@gmail.com>
2024-02-23 21:33:55 +0100

    ci: Fix the benchmarking tests

--- changelog/pre-changelog-gen.js
+++ changelog/pre-changelog-gen.js
@@ -23,7 +23,7 @@ exports.preTagGeneration = async function (tag) {
 	} | python3 format_benchmarks.py >> bench_over_time.csv
 
 	{
-		schema=$(cat test_files/pet_store_advanced.json)
+		schema=$(cat /home/runner/work/openapi-to-hurl/openapi-to-hurl/test_files/pet_store_advanced.json)
 
 		hyperfine --warmup 50 "echo $schema | /home/runner/work/openapi-to-hurl/openapi-to-hurl/target/release/openapi-to-hurl" -u millisecond --shell=none &&
 		git log --format="%H" -n 1 &&
@@ -31,9 +31,7 @@ exports.preTagGeneration = async function (tag) {
 	} | python3 format_benchmarks.py >> bench_over_time_stdin.csv
 
 	{
-		mkdir /test_hurl_files;
-
-		hyperfine --warmup 50 '/home/runner/work/openapi-to-hurl/openapi-to-hurl/target/release/openapi-to-hurl /home/runner/work/openapi-to-hurl/openapi-to-hurl/test_files/pet_store_advanced.json -o /test_hurl_files' -u millisecond --shell=none &&
+		hyperfine --warmup 50 '/home/runner/work/openapi-to-hurl/openapi-to-hurl/target/release/openapi-to-hurl /home/runner/work/openapi-to-hurl/openapi-to-hurl/test_files/pet_store_advanced.json -o /' -u millisecond --shell=none &&
 		git log --format="%H" -n 1 &&
 		echo ${tag};
 	} | python3 format_benchmarks.py >> bench_over_time_files.csv
